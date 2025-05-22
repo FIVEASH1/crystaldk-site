d19872439cc9f5d40ac080765ee8d15f1237f15b
FIVEASH1 <164813203+FIVEASH1@users.noreply.github.com>
2025-05-23 02:11:24 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,4 +4,7 @@ const menu = document.getElementById('menu');
 menuButton.addEventListener('click', () => {
   const isActive = menuButton.classList.toggle('active');
   menu.classList.toggle('hidden', !isActive);
+
+  // Меняем aria-hidden для доступности
+  menu.setAttribute('aria-hidden', !isActive);
 });
